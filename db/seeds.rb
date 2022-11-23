11e953d6148df9a736afc3089b2032b5c7d7389b
Olga Grigoryeva <33clouds@gmail.com>
2022-11-23 23:03:35 +0100

    Implement Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+
+[dishoom, pizza_east].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "restaurants created!"
+
+reviews = ["good", "bad", "exellent", "never again", "5 stars!"]
+
+reviews.each do |review|
+  new_rating = rand(0..5)
+  new_review = Review.new(rating: new_rating, content: review)
+  new_review.restaurant = Restaurant.all.sample
+  new_review.save
+end
+
+puts "reviews created!"
